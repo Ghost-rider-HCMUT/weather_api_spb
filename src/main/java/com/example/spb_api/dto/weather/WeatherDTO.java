7e6736637ca36b403b80356aa525e88481548ad4
HoangPhong <phongghoangg2001@gmail.com>
2024-09-09 08:20:10 +0700

    HoangPhong | weather_api | implement_docker

--- src/main/java/com/example/spb_api/dto/weather/WeatherDTO.java
+++ src/main/java/com/example/spb_api/dto/weather/WeatherDTO.java
@@ -1,5 +1,6 @@
 package com.example.spb_api.dto.weather;
 
+import com.example.spb_api.i18n.I18nUtil;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,43 +12,30 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class WeatherDTO {
-    @JsonProperty("city")
     private String city;
 
-    @JsonProperty("condition_weather")
     private String conditionWeather;
 
-    @JsonProperty("temp")
     private float temp;
 
-    @JsonProperty("wind_kph")
     private float windKph;
 
-    @JsonProperty("feelslike_c")
     private float feelsLike;
 
-    @JsonProperty("pressure_in")
     private float pressureIn;
 
-    @JsonProperty("humidity")
     private float humidity;
 
-    @JsonProperty("heatindex_c")
     private float heatIndex;
 
-    @JsonProperty("dewpoint_c")
     private float dewPoint;
 
-    @JsonProperty("precip_mm")
     private float precip;
 
-    @JsonProperty("gust_kph")
     private float gustKph;
 
-    @JsonProperty("is_day")
     private float isDay;
 
-    @JsonProperty("time")
     private String time;
 }
 
